1316af40dd389b08aaefe98d4d4223956b40f655
kirill krivosheev <36293477+KirillKrivosheev@users.noreply.github.com>
2020-02-19 21:31:42 +0300

    Update __init__.py

--- cigarette_butt_segmentation/lib/__init__.py
+++ cigarette_butt_segmentation/lib/__init__.py
@@ -5,7 +5,6 @@ from lib.html import get_html
 from lib.dataset import BasicDataset
 from lib.unet.unet_model import UNet
 from lib.dice_loss import dice_coeff
-from lib.data_vis import plot_img_and_mask
 from lib.eval import eval_net
 from lib.train import train_net
 from lib.predict import predict_img
